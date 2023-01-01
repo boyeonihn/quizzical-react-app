fcc8a89bdf156bc27b78f4ef6d70913eb69b7eda
Bo Yeon Ihn <boyeonihn@gmail.com>
2023-01-02 07:03:23 +0900

    feat: added useEffect to utilize fetch API for quiz questions

--- quizzical-app/src/Components/Quiz.js
+++ quizzical-app/src/Components/Quiz.js
@@ -2,8 +2,16 @@ import React from "react"
 import Question from "./Question"; 
 
 export default function Quiz(){
-    // API link is https://opentdb.com/api.php?amount=10&category=20&type=multiple
+    // API link is https://opentdb.com/api.php?amount=5&category=20&type=multiple
     // get questions from the Quiz API via fetch request
+
+    const [questionsData, setQuestionsData] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=5&category=20&type=multiple")
+            .then(res => res.json())
+            .then(data => setQuestionsData(data.results))
+    }, [])
     // the JSON results data.results 
     // question is stored in data.results[INDEX].question
     // correct answer is stored in data.results[INDEX].correct_answer and incorrect answer is stored in data.results[INDEX].incorrect_answer 
@@ -12,6 +20,7 @@ export default function Quiz(){
     
     return (
         <section>
+            <pre>{JSON.stringify(questionsData, null, 2)}</pre>
             <Question />
             <button>Check Answers</button>
         </section>
