e7cc3aaa83a76347630a9af247be0bf4b1896b6f
Bo Yeon Ihn <boyeonihn@gmail.com>
2023-01-02 07:07:11 +0900

    feat: rendered quiz question and answer choices on Quiz component

--- quizzical-app/src/Components/Question.js
+++ quizzical-app/src/Components/Question.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Question(){
+export default function Question(props){
     return (
         <div>
-            <h2>How would one say goodbye in Spanish?</h2>
+            <h2>{props.question}</h2>
             <p>Choice one</p>
             <p>Choice one</p>
             <p>Choice one</p>
--- quizzical-app/src/Components/Quiz.js
+++ quizzical-app/src/Components/Quiz.js
@@ -2,9 +2,6 @@ import React from "react"
 import Question from "./Question"; 
 
 export default function Quiz(){
-    // API link is https://opentdb.com/api.php?amount=5&category=20&type=multiple
-    // get questions from the Quiz API via fetch request
-
     const [questionsData, setQuestionsData] = React.useState([]);
 
     React.useEffect(() => {
@@ -12,16 +9,17 @@ export default function Quiz(){
             .then(res => res.json())
             .then(data => setQuestionsData(data.results))
     }, [])
-    // the JSON results data.results 
     // question is stored in data.results[INDEX].question
     // correct answer is stored in data.results[INDEX].correct_answer and incorrect answer is stored in data.results[INDEX].incorrect_answer 
 
     // somehow figure out how to mix the order of the answer choices; 
     
+    // first just pull out the QUESTIONS only from the data
+    const questions = questionsData.map( question => <Question question={question.question} />)
     return (
         <section>
             <pre>{JSON.stringify(questionsData, null, 2)}</pre>
-            <Question />
+            {questions}
             <button>Check Answers</button>
         </section>
     )
