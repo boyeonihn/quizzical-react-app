6f7b556d577f86347010b1cb89ca0157fea95963
Bo Yeon Ihn <boyeonihn@gmail.com>
2023-01-04 07:04:01 +0900

    refactor: refactored code so that special characters are rendered accurately

--- quizzical-app/src/Components/Quiz.js
+++ quizzical-app/src/Components/Quiz.js
@@ -9,11 +9,30 @@ export default function Quiz(props){
             .then(res => res.json())
             .then(data => setQuestionsData(data.results))
     }, [])
+
+    console.log(questionsData)
     
     // combine the answer choices in one array 
     const answerChoicesArray = questionsData.map( question => [...question.incorrect_answers, question.correct_answer]);
+    
+    // modified questions so that special characters are rendered correctly
+    const questionsArray = questionsData.map( question => {
+        const targetQuestion = question.question
+        if (targetQuestion.includes(`&#039;`)) {
+            return targetQuestion.replaceAll(`&#039;`, `'`)
+        }
+        else if (targetQuestion.includes(`&quot;`)) {
+            return targetQuestion.replaceAll(`&quot;`, `"`)
+        }
+        else if (targetQuestion.includes(`&amp;`)) {
+            return targetQuestion.replaceAll(`&amp;`, `&`)
+        }
+        else { 
+            return targetQuestion
+        }
+    })
     // map over the questions to create Question component
-    const questions = questionsData.map( (question, index) => <Question question={question.question} allChoices={answerChoicesArray[index]} answer={question.correct_answer} key={index} />)
+    const questions = questionsData.map( (question, index) => <Question question={questionsArray[index]} allChoices={answerChoicesArray[index]} answer={question.correct_answer} key={index} />)
     
     return (
         <section className="quiz--content">
